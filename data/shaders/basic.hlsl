175015db889b69e02e6f217a78d9d27de351fc38
goldroe <61571282+goldroe@users.noreply.github.com>
2024-03-22 22:49:14 -0500

    Create skinned shader and move basic

--- data/shaders/basic.hlsl
+++ data/shaders/basic.hlsl
@@ -1,6 +1,28 @@
-cbuffer Basic_Constants {
-    matrix mvp;
+struct Directional_Light {
+	float4 ambient;
+	float4 diffuse;
+	float4 specular;
+	float3 direction;
+	float pad;
+};
+
+struct Material {
     float4 color;
+    float4 ambience;
+    float4 diffuse;
+    float3 specular;
+    float pad;
+};
+
+cbuffer Constants_Per_Frame : register(b0) {
+    Directional_Light lights[3];
+    float3 eye_pos_w;
+};
+
+cbuffer Constants_Per_Obj : register(b1) {
+    matrix world;
+    matrix wvp;
+    Material material;
 };
 
 struct Vertex_In {
@@ -20,12 +42,12 @@ Texture2D texture_diffuse;
 
 Vertex_Out vs_main(Vertex_In vin) {
     Vertex_Out vout;
-    vout.position = mul(mvp, float4(vin.position, 1));
+    vout.position = mul(wvp, float4(vin.position, 1));
     vout.normal = vin.normal;
     vout.uv = vin.uv;
     return vout;
 }
 
 float4 ps_main(Vertex_Out pin) : SV_TARGET {
-    return color * texture_diffuse.Sample(model_sampler, pin.uv);
+    return material.color * texture_diffuse.Sample(basic_sampler, pin.uv);
 }
--- data/shaders/skinned_model.hlsl
+++ data/shaders/skinned_model.hlsl
@@ -1,12 +1,15 @@
 #define MAX_BONES 100
 
-cbuffer Skinned_Constants {
-    matrix mvp;
-    matrix bone_matrices[MAX_BONES];
-    float4 color;
+cbuffer constants_Per_Frame {
+    float3 eye_pos_w;
 };
 
-struct Skinned_Vertex_In {
+cbuffer Constants_Per_Obj {
+    matrix wvp;
+    matrix bone_matrices[MAX_BONES];
+};
+
+struct Vertex_In {
     float3 position : POSITION;
     float3 normal : NORMAL;
     float2 uv : TEXCOORD;
@@ -23,14 +26,14 @@ struct Vertex_Out {
 SamplerState model_sampler;
 Texture2D texture_diffuse;
 
-Vertex_Out vs_main(Skinned_Vertex_In vin) {
+Vertex_Out vs_main(Vertex_In vin) {
     Vertex_Out vout;
-    vout.position = mul(mvp, float4(vin.position, 1.0));
+    vout.position = mul(wvp, float4(vin.position, 1.0));
     vout.normal = vin.normal;
     vout.uv = vin.uv;
     return vout;
 }
 
-float4 ps_main(Vertex_Out pin) {
+float4 ps_main(Vertex_Out pin) : SV_TARGET {
     return texture_diffuse.Sample(model_sampler, pin.uv);
 }
